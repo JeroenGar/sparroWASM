10c7913105d552f8d8d2544e959f5d32a6ec8944
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2025-08-14 00:53:57 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import wasm from "vite-plugin-wasm";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sparrowasm",
+  base: "/sparroWASM",
   plugins: [
     react(),
     svgr({
