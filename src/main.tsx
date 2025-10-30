c5f42808a4c8276470b62a001994ef12489ed717
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2025-10-30 15:57:59 +0100

    refactor: switch from BrowserRouter to HashRouter for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App.tsx";
 import Demo from "./Demo";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter basename="/sparroWASM">
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/demo" element={<Demo />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 );
